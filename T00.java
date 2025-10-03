a299b2e97d49d5349ec3a526b7eeda9207e0a61b
Gabz1boi <gabrielignasiussinaga000@gmail.com>
2025-10-03 09:36:38 +0700

    TES 1

---
+++ T00.java
@@ -0,0 +1,46 @@
+//12S25021-Gabriel Ignasius Sinaga 12S25030-Nina Theresia Tarigan
+
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x, y;
+
+        x = Integer.parseInt(input.nextLine());
+        y = Integer.parseInt(input.nextLine());
+        if (x % 2 == 0) {
+            System.out.println(" Bilangan pertama: Genap ");
+        } else {
+            System.out.println(" Bilangan pertama: Ganjil ");
+        }
+        if (y % 2 == 0) {
+            System.out.println(" Bilangan kedua: Genap ");
+        } else {
+            System.out.println(" Bilangan kedua: Ganjil");
+        }
+        if (x > y) {
+            System.out.println(" Bilangan pertama lebih besar ");
+        }
+        if (x < y) {
+            System.out.println(" Bilangan kedua lebih besar ");
+        }
+        if (y == x) {
+            System.out.println(" Kedua Bilangan sama besar ");
+        }
+        if (x % 2 == 0 && y % 2 == 0) {
+            System.out.println(" Hasil Penjumlahan :" + x + y);
+        }
+        if (x % 2 != 0 && y % 2 != 0) {
+            System.out.println("Hasil Perkalian: " + x * y);
+        }
+        if (x % 2 != 0 && y % 2 == 0) {
+            System.out.println("Berbeda Jenis");
+        }
+        if (x % 2 == 0 && y % 2 != 0) {
+            System.out.println("Berbeda Jenis");
+        }
+    }
+}
